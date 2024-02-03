73529c5637f81f37e2ed3b6ea56ad72cea17478d
sufigueroa87 <sufigueroa@proton.me>
2024-02-03 01:47:15 +0100

    añado ejercicios del apartado part01.05_conditional_statements_parteA

---
+++ part01/05_conditional_statements/01_orwell.py
@@ -0,0 +1,14 @@
+# Please write a program which asks the user for an integer number. The program should print out "Orwell" if the
+# number is exactly 1984, and otherwise do nothing.
+# Sample output
+#
+# Please type in a number: 2020
+# Sample output
+#
+# Please type in a number: 1984
+# Orwell
+
+number = int(input("Please type in a number: "))
+
+if number == 1984:
+    print("Orwell")
---
+++ part01/05_conditional_statements/05_calculator.py
@@ -0,0 +1,39 @@
+#
+# Please write a program which asks the user for two numbers and an operation. If the operation is add, multiply or
+# subtract, the program should calculate and print out the result of the operation with the given numbers. If the user
+# types in anything else, the program should print out nothing.
+#
+# Some examples of expected behaviour:
+# Sample output
+#
+# Number 1: 10
+# Number 2: 17
+# Operation: add
+#
+# 10 + 17 = 27
+# Sample output
+#
+# Number 1: 4
+# Number 2: 6
+# Operation: multiply
+#
+# 4 * 6 = 24
+# Sample output
+#
+# Number 1: 4
+# Number 2: 6
+# Operation: subtract
+#
+# 4 - 6 = -2
+
+number1 = int(input("Number 1: "))
+number2 = int(input("Number 2: "))
+operation = input("Enter operation: ")
+
+if operation == "add":
+    print(f"{number1} + {number2} = {number1+number2}")
+elif operation == "multiply":
+    print(f"{number1} * {number2} = {number1*number2}")
+elif operation == "subtract":
+    print(f"{number1} - {number2} = {number1-number2}")
+
